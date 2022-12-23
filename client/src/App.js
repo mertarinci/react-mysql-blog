d90533f8ffd859fe298acdad6ca063923a20d925
mertarinci <mertarinci96@gmail.com>
2022-12-23 16:22:51 +0100

    skeleton of project

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,68 @@
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+
+import "./style.scss"
+
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+import Single from "./pages/Single"
+import Home from "./pages/Home"
+import Write from "./pages/Write"
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (<>
+    <Navbar />
+    <Outlet />
+    <Footer />
+  </>)
+}
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/post/:id",
+        element: <Single />
+      },
+      {
+        path: "/write",
+        element: <Write />
+      },
+
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
 function App() {
   return (
-    <div>
-      Hello world!
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
 
+
 export default App;
+
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className="auth">
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button>Login </button>
+        <span>
+          Don't you have an account? <Link></Link>
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
