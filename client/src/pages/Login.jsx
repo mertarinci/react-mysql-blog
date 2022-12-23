0d4629e24142295118a684feecaa337292056ee9
mertarinci <mertarinci96@gmail.com>
2022-12-23 16:33:15 +0100

    add login register

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -9,8 +9,9 @@ const Login = () => {
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
         <button>Login </button>
+        <p>This is an error!</p>
         <span>
-          Don't you have an account? <Link></Link>
+          Don't you have an account? <Link to="/register">Register</Link>
         </span>
       </form>
     </div>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="auth">
+      <h1>Register</h1>
+      <form>
+        <input required type="text" placeholder="Email" />
+        <input required type="text" placeholder="Username" />
+        <input required type="password" placeholder="Password" />
+        <button>Login </button>
+        <p>This is an error!</p>
+        <span>
+          Do you have an account? <Link to="/login">Login</Link>
+        </span>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
