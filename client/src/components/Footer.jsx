b327fe92083223629fdeb8f9ed1146d6eec77431
mertarinci <mertarinci96@gmail.com>
2022-12-24 13:27:37 +0100

    add footer and navbar

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import Logo from "../img/logo.png";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <span>
+        Made by <b>Mert A.</b> with <b>ReactJS</b>
+      </span>
+      <img src={Logo} alt="" />
+    </div>
+  );
 };
 
 export default Footer;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../img/logo.png";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="container">
-        <div className="logo">Logo</div>
-        <div className="links">Links</div>
+        <div className="logo">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="links">
+          <Link className="link" to="/?cat=art">
+            <h6>ART</h6>
+          </Link>
+          <Link className="link" to="/?cat=science">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className="link" to="/?cat=tech">
+            <h6>TECH</h6>
+          </Link>
+          <Link className="link" to="/?cat=design">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className="link" to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link>
+          <Link className="link" to="/?cat=cinema">
+            <h6>CINEMA</h6>
+          </Link>
+          <div className="userBar">
+            <span>Username</span>
+            <span>Logout</span>
+            <span className="write">
+              <Link className="link" to="/write">
+                Write
+              </Link>
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
